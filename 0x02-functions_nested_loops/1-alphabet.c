65fa24ac4019dc8c64adf3fba8dbbd852fd25ea4
SAMIYISE <samiyisehak@gmail.com>
2022-07-06 08:43:24 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet
+ *
+ * return:0
+ *
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x)
+	}
+	_putchar('\n')
+}
