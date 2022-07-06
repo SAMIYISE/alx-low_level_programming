6c75f5ed7a17c6af965d08d0cd3135fb67789662
SAMIYISE <samiyisehak@gmail.com>
2022-07-06 14:10:41 -0700

    alphabet

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,13 +1,10 @@
 #include "main.h"
 
 /**
- * void print_alphabet - function
+ * main - check code
  *
- * main - check the code
- *
- * Return: Always 0.
+ * return:0
  */
-void print_alphabet(void);
 int main(void)
 {
 	print_alphabet();
