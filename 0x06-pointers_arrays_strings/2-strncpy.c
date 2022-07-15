e3e168161e041f21a7254789362a580c09251651
SAMIYISE <samiyisehak@gmail.com>
2022-07-14 19:30:49 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - function to copy string
+ *
+ * @dest: destination
+ * @src: source
+ * @n: amount
+ *
+ * Return: destination value
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for ( i = 0; i < n && src[i] !='\0'; i++)
+	{
+		dest[i] = src[i]
+	}
+	for (; i < n; i++)
+	{
+		dest[i] != '\0';
+	}
+	return (dest);
+}
