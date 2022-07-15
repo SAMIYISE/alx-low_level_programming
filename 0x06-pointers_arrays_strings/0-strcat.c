fd40305db8dfbdc412a97ec9af3f05d694eb83b3
SAMIYISE <samiyisehak@gmail.com>
2022-07-14 17:04:52 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * strcat - concats 2 arrays
+ *
+ * @dest: destination of concat
+ * @src: source of array
+ *
+ * Return: char value
+ */
+char *_strcat(char *dest, char *src)
+{
+	int x;
+	int y;
+
+	for (y = 0; dest[y] != '\0'; y++)
+	{}
+	for (x = 0; src[x] != '\0'; x++)
+	{
+		dest[y + x] = src[x];
+	}
+	dest[y + x] = '\0';
+
+	return (dest);
+}
