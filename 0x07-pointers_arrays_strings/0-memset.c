6e78c74e41803645b7143128c1a9f0608213eaca
SAMIYISE <samiyisehak@gmail.com>
2022-07-19 16:00:07 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - set memory
+ * @s: array
+ * @b: value to set
+ * @n: amount of times
+ *
+ * Return: value of s
+ */
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
